2503ef8e7825397ba8517e1cb708b9ac9c14847d
24290by <24290by@hvhs.school.nz>
2026-07-23 10:28:12 +0000

    Jul 23, 2026, 10:28 AM

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ function getFormInput() {
     let order = document.getElementById("nameField").value.trim().toLowerCase();
     let money = Number(document.getElementById("pocketmoneyField").value);
 
+    // Check if the order is valid first
+    if (order !== "2p") {
+        OUTPUT.innerHTML = "<h2>Order not created.</h2>";
+        return;
+    }
+
     OUTPUT.innerHTML = `
         <h2>Receipt</h2>
         <p><b>Name:</b> ${customerName}</p>
@@ -13,28 +19,19 @@ function getFormInput() {
         <p><b>Money:</b> $${money}</p>
     `;
 
-    // Check if the order is valid
-    if (order !== "2p") {
-        OUTPUT.innerHTML += `
-            <p style="color:red;"><b>Invalid item.</b></p>
-            <p>Order not created.</p>
-        `;
-        return; // Stop the function here
-    }
-
     // Check if the customer has enough money
     if (money >= 25) {
         let change = money - 25;
 
         OUTPUT.innerHTML += `
-            <p style="color:green;">Successfully purchased burger!</p>
+            <p>Successfully purchased burger.</p>
             <p><b>Change:</b> $${change}</p>
         `;
     } else {
         let needed = 25 - money;
 
         OUTPUT.innerHTML += `
-            <p style="color:red;">Insufficient funds.</p>
+            <p>Insufficient funds.</p>
             <p>You need $${needed} more to buy the burger.</p>
         `;
     }
