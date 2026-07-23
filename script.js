f1f398c1b764b0e3bc7bbcb21a41f527c7aab97e
24290by <24290by@hvhs.school.nz>
2026-07-23 21:43:57 +0000

    Jul 23, 2026, 9:43 PM

--- script.js
+++ script.js
@@ -2,60 +2,60 @@ let OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 
 function getFormInput() {
 
-    let customerName = document.getElementById("customerName").value;
-    let order = document.getElementById("nameField").value.trim().toLowerCase();
-    let money = Number(document.getElementById("pocketmoneyField").value);
+let customerName = document.getElementById("customerName").value;
+let order = document.getElementById("nameField").value.trim().toLowerCase();
+let money = Number(document.getElementById("pocketmoneyField").value);
 
-    OUTPUT.innerHTML = `
-        <h2>Receipt</h2>
-        <p><b>Name:</b> ${customerName}</p>
-        <p><b>Order:</b> ${order}</p>
-        <p><b>Money:</b> $${money}</p>
-    `;
+OUTPUT.innerHTML = `
+<h2>Receipt</h2>
+<p><b>Name:</b> ${customerName}</p>
+<p><b>Order:</b> ${order}</p>
+<p><b>Money:</b> $${money}</p>
+`;
 
-    // Burger
-    if (order === "2p") {
-        if (money >= 25) {
-            let change = money - 25;
 
-            OUTPUT.innerHTML += `
-                <p>Successfully purchased burger!</p>
-                <p><b>Change:</b> $${change}</p>
-            `;
-        } else {
-            let needed = 25 - money;
+if (order === "2p") {
+if (money >= 25) {
+let change = money - 25;
 
-            OUTPUT.innerHTML += `
-                <p>Insufficient funds.</p>
-                <p>You need $${needed} more to buy the burger.</p>
-            `;
-        }
-    }
+OUTPUT.innerHTML += `
+<p>Successfully purchased burger!</p>
+<p><b>Change:</b> $${change}</p>
+`;
+} else {
+let needed = 25 - money;
 
-    // Drink
-    else if (order === "4p") {
-        if (money >= 10) {
-            let change = money - 10;
+OUTPUT.innerHTML += `
+<p>Insufficient funds.</p>
+<p>You need $${needed} more to buy the burger.</p>
+`;
+}
+}
 
-            OUTPUT.innerHTML += `
-                <p>Successfully purchased drink!</p>
-                <p><b>Change:</b> $${change}</p>
-            `;
-        } else {
-            let needed = 10 - money;
 
-            OUTPUT.innerHTML += `
-                <p>Insufficient funds.</p>
-                <p>You need $${needed} more to buy the drink.</p>
-            `;
-        }
-    }
+else if (order === "4p") {
+if (money >= 10) {
+let change = money - 10;
 
-    // Invalid order
-    else {
-        OUTPUT.innerHTML += `
-            <p><b>Invalid item.</b></p>
-            <p>Order not created.</p>
+OUTPUT.innerHTML += `
+<p>Successfully purchased drink!</p>
+<p><b>Change:</b> $${change}</p>
+`;
+} else {
+let needed = 10 - money;
+
+OUTPUT.innerHTML += `
+<p>Insufficient funds.</p>
+<p>You need $${needed} more to buy the drink.</p>
+`;
+}
+}
+
+
+else {
+OUTPUT.innerHTML += `
+<p><b>Invalid item.</b></p>
+<p>Order not created.</p>
         `;
     }
 }
